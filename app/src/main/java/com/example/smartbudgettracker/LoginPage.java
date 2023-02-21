a75b856066fdc0fe57a344e98e786896d1e37067
Ashwin Pai <ashwinvsd@gmail.com>
2023-02-22 01:08:14 +0530

    Login & Home Page merged

---
+++ app/src/main/java/com/example/smartbudgettracker/LoginPage.java
@@ -0,0 +1,31 @@
+package com.example.smartbudgettracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginPage extends AppCompatActivity {
+
+
+    Button loginbutton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+        loginbutton = findViewById(R.id.login_button);
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(view.getContext(),MainActivity.class));
+            }
+        });
+
+
+
+
+    }
+}
